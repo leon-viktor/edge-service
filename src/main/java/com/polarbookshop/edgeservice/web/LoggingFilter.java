517049385dcc43bc21f35117c7827517386c169b
Viktor <leon4cloud@gmail.com>
2025-06-14 16:26:49 +0300

    - add LoggingFilter

---
+++ src/main/java/com/polarbookshop/edgeservice/web/LoggingFilter.java
@@ -0,0 +1,25 @@
+package com.polarbookshop.edgeservice.web;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+@Component
+@Order(-1)
+public class LoggingFilter implements GlobalFilter {
+
+    private static final Logger log = LoggerFactory.getLogger(LoggingFilter.class);
+
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+        var request = exchange.getRequest();
+        log.info("Incoming Request: method={}, path={}, headers={}", request.getMethod(), request.getURI(), request.getHeaders());
+        return chain.filter(exchange);
+    }
+
+}
